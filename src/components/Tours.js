4613ab5d82d8552c08b83375a69f430cecb65911
Sanket Darunkar <sanketdarunkar111@gmail.com>
2025-06-19 17:59:44 +0530

    adding new Project of React.js

---
+++ src/components/Tours.js
@@ -0,0 +1,19 @@
+import Card from "./Card";
+function Tours({tours,removeTour}){
+        return (
+            <div>
+                <div className="container">
+                <h2 className="title">Plan With Sanket</h2>
+                </div>
+                <div className="cards">
+                    {
+                        tours.map( (tour) => {
+                            return <Card {...tour} key={tour.id} removeTour={removeTour }> </Card>
+                        })
+                    }
+                </div>
+            </div>
+        );
+
+}
+export default Tours;
